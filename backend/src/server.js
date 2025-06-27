fd025b9e337466c987e436459767c2cb4304b4ad
ihab hafda <ihabhafda@gmail.com>
2025-06-27 23:01:46 +0300

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,16 +11,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middleware setup
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-); // Apply rate limiting middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  ); // Apply rate limiting middleware
+}
+
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(rateLimiter); // Apply rate limiting middleware
-app.use(cors()); // Enable CORS for all origins
 
 // app.use((req, res, next) => {
 //   console.log(`Req.method is ${req.method} & Req URl is '${req.url}'`);
@@ -27,6 +31,16 @@ app.use(cors()); // Enable CORS for all origins
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  // Serve static files from the React app
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // Handle any requests that don't match the above routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
